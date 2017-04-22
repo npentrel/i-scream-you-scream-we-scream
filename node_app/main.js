786e1cd22b462f7aef5440fdda2a342039693880
npentrel <npentrel@gmail.com>
2017-04-22 22:11:12 +0100

    full screen

--- node_app/main.js
+++ node_app/main.js
@@ -1,6 +1,15 @@
 // Credits to: http://www.lessmilk.com/tutorial/flappy-bird-phaser-1
 
 // Create our 'main' state that will contain the game
+
+var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    x = w.innerWidth || e.clientWidth || g.clientWidth,
+    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+
 var mainState = {
   preload: function() {
     // Load the bird sprite
@@ -35,14 +44,15 @@ var mainState = {
 
     this.timer = game.time.events.loop(3000, this.addRowOfPipes, this);
 
+    this.score_start = 0;
     this.score = 0;
-    this.labelScore = game.add.text(20, 20, "0",
+    this.labelScore = game.add.text(30, 30, "0",
         { font: "30px Arial", fill: "#ffffff" });
   },
   update: function() {
     // If the bird is out of the screen (too high or too low)
     // Call the 'restartGame' function
-    if (this.bird.y < 0 || this.bird.y > 490)
+    if (this.bird.y < 0 || this.bird.y > y)
         this.restartGame();
     game.physics.arcade.overlap(
         this.bird, this.pipes, this.restartGame, null, this);
@@ -80,21 +90,26 @@ var mainState = {
   addRowOfPipes: function() {
       // Randomly pick a number between 1 and 5
       // This will be the hole position
-      var hole = Math.floor(Math.random() * 5) + 1;
+      var hole = Math.floor(Math.random() * (((y)/60) - 4)) + 1;
 
       // Add the 6 pipes
       // With one big hole at position 'hole' and 'hole + 1'
-      for (var i = 0; i < 8; i++)
-          if (i != hole && i != hole + 1)
-              this.addOnePipe(400, i * 60 + 10);
-
-      this.score += 1;
+      console.log(y);
+      console.log((y)/60);
+      for (var i = 0; i < ((y)/60); i++)
+          if (i != hole && i != hole + 1 && i != hole + 2 && i != hole + 3 )
+              this.addOnePipe(x - 15, i * 60 + 10);
+      if (this.score_start > 1) {
+        this.score += 1;
+      } else {
+        this.score_start++;
+      }
       this.labelScore.text = this.score;
   },
 };
 
 // Initialize Phaser, and create a 400px by 490px game
-var game = new Phaser.Game(400, 490);
+var game = new Phaser.Game(x - 15, y - 15);
 
 // Add the 'mainState' and call it 'main'
 game.state.add('main', mainState);
