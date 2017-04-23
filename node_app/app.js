453aa64662fddea0ab3d5d51e9e4eb041a8262a6
Pandelis Zembashis <info@pandelis.me>
2017-04-23 07:29:17 +0100

    might aswell

--- node_app/app.js
+++ node_app/app.js
@@ -7,6 +7,9 @@ const express = require('express');
 const app = express();
 const env = require('node-env-file');
 
+var randomUsername = require('./randos');
+
+
 env(__dirname + '/.env');
 
 const MODERATOR = process.env.NAOMI_SKYPE;
@@ -38,7 +41,20 @@ app.post('/voice', (request, response) => {
   // Use the Twilio Node.js SDK to build an XML response
   let twiml = new twilio.TwimlResponse();
 
-  // Start with a <Dial> verb
+  if(request.body.To) {
+    console.log('executed')
+    console.log('calling ' + request.body.To)
+    twiml.dial({ callerId: process.env.TWILIO_PHONE_NUMBER}, function() {
+      // wrap the phone number or client name in the appropriate TwiML verb
+      // by checking if the number given has only digits and format symbols
+      if (/^[\d\+\-\(\) ]+$/.test(request.body.To)) {
+        this.number(request.body.To);
+      } else {
+        this.client(request.body.To);
+      }
+    });
+  } else {
+    // Start with a <Dial> verb
   twiml.dial(function(dialNode) {
     // If the caller is our MODERATOR, then start the conference when they
     // join and end the conference when they leave
@@ -58,10 +74,30 @@ app.post('/voice', (request, response) => {
       });
     }
   });
+  }
+
+ 
 
   // Render the response as XML in reply to the webhook request
   response.type('text/xml');
   response.send(twiml.toString());
+});
+
+
+app.get('/token', function(request, response) {
+  var identity = 'WebRTCWebCLient';
+  
+  var capability = new twilio.Capability(process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_AUTH_TOKEN);
+  capability.allowClientOutgoing(process.env.TWILIO_TWIML_APP_SID);
+  capability.allowClientIncoming(identity);
+  var token = capability.generate();
+
+  // Include identity and token in a JSON response
+  response.send({
+    identity: identity,
+    token: token
+  });
 });
 
 // Create an HTTP server and listen for requests on port 3000
