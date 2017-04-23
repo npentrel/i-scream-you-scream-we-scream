0f0c7ced584d30f6d4c9597c41953840a9b6d92a
npentrel <npentrel@gmail.com>
2017-04-23 10:48:36 +0100

    easier

--- node_app/main.js
+++ node_app/main.js
@@ -90,7 +90,7 @@ var mainState = {
   addRowOfPipes: function() {
       // Randomly pick a number between 1 and 5
       // This will be the hole position
-      var hole = Math.floor(Math.random() * (((y)/60) - 6)) + 1;
+      var hole = Math.floor(Math.random() * (((y)/60) - 9)) + 1;
 
       // Add the 6 pipes
       // With one big hole at position 'hole' and 'hole + 1'
